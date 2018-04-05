cb1c2de00e47f731fca21238c1d0cbbbeb1cb239
Guillaume <guillaume-cornet07@hotmail.fr>
2018-04-05 11:46:08 +0200

    PDO & Update fun

--- KandT/includes/function.php
+++ KandT/includes/function.php
@@ -1,11 +1,5 @@
 <?php
 /**
- * Created by PhpStorm.
- * @author Yann Le Scouarnec <bunkermaster@gmail.com>
- * Date: 28/02/2018
- * Time: 16:54
- */
-/**
  * @param string $slug
  * @param string $navTitle
  * @return void
